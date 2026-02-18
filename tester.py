77704178778e7bd4068f141cae736f765bee301a
Bogumil Chazy <bogumiljan.chazy@gmail.com>
2026-02-18 11:16:01 +0100

    zmiany Darka

--- tester.py
+++ tester.py
@@ -5,4 +5,5 @@ Ploter = LabVIEW_python_ploter.PlotEngine()
 
 Ploter.plot_line([0, 1, 2, 3, 4, 5], [2, 3, 2, 4, 2, 7], "{\"xlabel\":\"asdasd\", \"ylabel\":\"svcse\"}", r"C:\Users\bogum\OneDrive\Obrazy\test\efsef.png")
 print("SDF")
+# vjsivhsuoehvwesd
 
